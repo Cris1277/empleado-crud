caed7fbadfe24a1d161a7c6244b9df0714c04c9b
Cris1277 <aztecafran@gmail.com>
2025-03-16 21:49:02 +0100

    Update cambiar-rol.js

--- backend/routes/cambiar-rol.js
+++ backend/routes/cambiar-rol.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const pool = require("../db");
-const db = require("../db");
+//const db = require("../db");
 const authMiddleware = require("./authMiddleware");
 const adminMiddleware = require("./adminMiddleware");
 
